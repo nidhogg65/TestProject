d8771c21e5129bcc424501acfd7025ef7ff54f93
Nidhogg <nidhogg65@gmail.com>
2020-08-27 21:09:26 +0300

    initialize git rep

---
+++ src/main/java/com/nidhogg/studyspringproject/StudySpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nidhogg.studyspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nidhogg/studyspringproject/StudySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidhogg.studyspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
