d803bf600f83a128f46196d0cc8c21b75e249b80
Nidhogg <nidhogg65@gmail.com>
2021-01-12 15:00:15 +0300

    fix remove uuid from CreateAccountDto

--- src/main/java/com/nidhogg/studyspringproject/dto/account/CreateAccountDto.java
+++ src/main/java/com/nidhogg/studyspringproject/dto/account/CreateAccountDto.java
@@ -1,20 +1,16 @@
 package com.nidhogg.studyspringproject.dto.account;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nidhogg.studyspringproject.domain.model.account.AccountCurrency;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class CreateAccountDto {
 
     @NotNull
-    private AccountCurrency currency;
-    @JsonProperty("user_id")
-    private String userUuid;
+    private final AccountCurrency currency;
+
 }
