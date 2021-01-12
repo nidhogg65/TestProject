d00460cd923df09463e8b1c55af590e240bb6a75
Nidhogg <nidhogg65@gmail.com>
2021-01-12 14:58:23 +0300

    fix exception message

--- src/main/java/com/nidhogg/studyspringproject/application/exception/EntityNotFoundException.java
+++ src/main/java/com/nidhogg/studyspringproject/application/exception/EntityNotFoundException.java
@@ -3,6 +3,6 @@ package com.nidhogg.studyspringproject.application.exception;
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(Class<?> entityClass) {
-        super(entityClass.getSimpleName() + " with such identificator does not exist.");
+        super(entityClass.getSimpleName() + " with such identifier does not exist.");
     }
 }
